4e75acd7e4cdfb2635f8a8878d897069e0c668ff
siznax <sisney@panix.com>
2012-02-13 08:56:33 -0800

    added metadata.

--- flashcards.py
+++ flashcards.py
@@ -4,9 +4,20 @@ class Flashcards:
 
     default="ja"
 
-    def __init__(self,deckfile="decks/c001.json"):
+    def __init__(self,deckfile="decks/ja-a-1-1.json"):
         import json
-        self.deck = json.load(open(deckfile,'r'))
+        j = json.load(open(deckfile,'r'))
+        self.set_meta(j,deckfile)
+        self.deck = j["cards"]
+
+    def set_meta(self,json,deckfile):
+        fname = deckfile.split('-')
+        self.meta = {'source' :json["source"],
+                     'creator':json["creator"],
+                     'version':json["version"],
+                     'ref'    :fname[1],
+                     'level'  :fname[2],
+                     'section':fname[3].split('.')[0]}
 
     def putcard(self,ini):
         if not ini:
@@ -16,5 +27,8 @@ class Flashcards:
             base=".."
         import random
         card = self.deck[random.randint(0,len(self.deck)-1)]
-        return render_template("card.html",base=base,ini=ini,card=card)
-
+        return render_template("card.html",
+                               base=base,
+                               ini=ini,
+                               card=card,
+                               meta=self.meta)
