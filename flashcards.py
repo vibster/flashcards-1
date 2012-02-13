d00b1a51eaae6f7b2bd20bcf22a764f70510a68b
siznax <sisney@panix.com>
2012-02-13 11:12:38 -0800

    refactored for /<dict>/<clue>

--- flashcards.py
+++ flashcards.py
@@ -2,33 +2,69 @@ from flask import render_template
 
 class Flashcards:
 
-    default="ja"
+    ini_deck = "ja-a-1-1"
 
-    def __init__(self,deckfile="decks/ja-a-1-1.json"):
+    def __init__(self,deck,clue):
+        self.set_deck(deck)
+        self.set_card()
+        self.set_meta()
+        self.clue = clue or self.deck.split('-')[0]
+        if deck and clue:
+            self.base = "../.."
+        if (deck and not clue) or (clue and not deck):
+            self.base = ".."
+        if not deck and not clue:
+            self.base = "."
+
+    def set_deck(self,deck):
+        self.deck = deck or self.ini_deck
+        import os
+        deck_file = "decks/%s.json" % (self.deck)
+        if not os.path.exists(deck_file):
+            self.json = None
+            self.cards = None
+            self.lang = None
+            return 
         import json
-        j = json.load(open(deckfile,'r'))
-        self.set_meta(j,deckfile)
-        self.deck = j["cards"]
+        self.json = json.load(open(deck_file,'r'))
+        self.cards = self.json["cards"]
+        self.lang = self.deck.split('-')[0]
 
-    def set_meta(self,json,deckfile):
-        fname = deckfile.split('-')
-        self.meta = {'source' :json["source"],
-                     'creator':json["creator"],
-                     'version':json["version"],
-                     'ref'    :fname[1],
-                     'level'  :fname[2],
-                     'section':fname[3].split('.')[0]}
-
-    def putcard(self,ini):
-        if not ini:
-            ini=self.default
-            base="."
-        else:
-            base=".."
+    def set_card(self):
         import random
-        card = self.deck[random.randint(0,len(self.deck)-1)]
+        if not self.cards:
+            self.card = None
+            return 
+        self.card = self.cards[random.randint(0,len(self.deck)-1)]
+
+    def dict_href(self):
+        import urllib
+        if self.lang == "ja":
+            base = "http://jisho.org"
+            arg  = urllib.quote(self.card[self.lang].encode('utf-8'))
+            args = "words?jap=%s&eng=&dict=edict" % (arg)
+        else:
+            return None
+        return "%s/%s" % (base,args)
+
+    def set_meta(self):
+        if not self.json:
+            self.meta = None
+            return 
+        self.meta = {'deck'   :self.deck,
+                     'source' :self.json["source"],
+                     'creator':self.json["creator"],
+                     'version':self.json["version"],
+                     'srcref' :self.deck.split('-')[1],
+                     'level'  :self.deck.split('-')[2],
+                     'section':self.deck.split('-')[3],
+                     'dict'   :self.dict_href()}
+        
+    def putcard(self):
+        if not self.card:
+            return render_template('404.html'), 404
         return render_template("card.html",
-                               base=base,
-                               ini=ini,
-                               card=card,
-                               meta=self.meta)
+                               clue = self.clue,
+                               base = self.base,
+                               card = self.card,
+                               meta = self.meta)
--- index.py
+++ index.py
@@ -1,14 +1,15 @@
 from flashcards import Flashcards
-fc = Flashcards()
 
 from flask import Flask
 from flask import make_response
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/<ini>/')
-def index(ini=None):
-    return fc.putcard(ini)
+@app.route('/<deck>/')
+@app.route('/<deck>/<clue>/')
+def index(deck=None,clue=None):
+    fc = Flashcards(deck,clue)
+    return fc.putcard()
 
 if __name__ == '__main__':
     app.debug = True
